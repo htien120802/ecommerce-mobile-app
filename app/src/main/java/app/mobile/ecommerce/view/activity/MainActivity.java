848a846234933df32b46b2c9e83f7c9fb368b32f
htien120802 <dhtien120802@gmail.com>
2023-04-22 20:23:22 +0700

    create app

---
+++ app/src/main/java/app/mobile/ecommerce/view/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package app.mobile.ecommerce.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.mobile.ecommerce.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
